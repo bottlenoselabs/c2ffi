3ebe22242004ff869294a029a8f251452ac0a2c1
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2024-01-30 16:55:13 -0500

    Add scaffold

---
+++ src/cs/production/c2json.Tool/Program.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
+
+Console.WriteLine("Hello, World!");
---
+++ src/cs/tests/c2json.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Bottlenose Labs Inc. (https://github.com/bottlenoselabs). All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the Git repository root directory for full license information.
+
+namespace c2json.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
