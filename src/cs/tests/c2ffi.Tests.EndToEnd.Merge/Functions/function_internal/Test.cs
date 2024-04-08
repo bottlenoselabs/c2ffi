3bf64acb06e01413b3459cbd6e84bcfa35d34aa3
Lucas Girouard-Stranks <519592+lithiumtoast@users.noreply.github.com>
2024-04-08 14:10:15 -0400

    Add tests

--- src/cs/tests/c2ffi.Tests.EndToEnd.Merge/Functions/function_internal/Test.cs
+++ src/cs/tests/c2ffi.Tests.EndToEnd.Merge/Functions/function_internal/Test.cs
@@ -3,57 +3,54 @@
 
 using c2ffi.Tests.Library.Models;
 using FluentAssertions;
+using Xunit;
 
 #pragma warning disable CA1707
 
-namespace c2ffi.Tests.EndToEnd.Extract.Functions.function_internal;
+namespace c2ffi.Tests.EndToEnd.Merge.Functions.function_internal;
 
-public class Test : ExtractFfiTest
+public class Test : MergeFfisTest
 {
     private const string FunctionName = "function_internal";
 
     [Fact]
     public void Function()
     {
-        var ffis = GetTargetPlatformFfis(
-            $"src/c/tests/functions/{FunctionName}/config.json");
-        Assert.True(ffis.Length > 0);
+        var ffi = GetCrossPlatformFfi(
+            $"src/c/tests/functions/{FunctionName}/ffi");
 
-        foreach (var ffi in ffis)
-        {
-            Function1DoesNotExist(ffi);
-            Function2DoesExist(ffi);
-            Function3DoesNotExist(ffi);
-            Function4DoesNotExist(ffi);
-            Function5DoesExist(ffi);
-        }
+        Function1DoesNotExist(ffi);
+        Function2DoesExist(ffi);
+        Function3DoesNotExist(ffi);
+        Function4DoesNotExist(ffi);
+        Function5DoesExist(ffi);
     }
 
-    private void Function1DoesNotExist(CTestFfiTargetPlatform ffi)
+    private void Function1DoesNotExist(CTestFfiCrossPlatform ffi)
     {
         var function = ffi.TryGetFunction("function_internal_1");
         function.Should().BeNull();
     }
 
-    private void Function2DoesExist(CTestFfiTargetPlatform ffi)
+    private void Function2DoesExist(CTestFfiCrossPlatform ffi)
     {
         var function = ffi.TryGetFunction("function_internal_2");
         function.Should().NotBeNull();
     }
 
-    private void Function3DoesNotExist(CTestFfiTargetPlatform ffi)
+    private void Function3DoesNotExist(CTestFfiCrossPlatform ffi)
     {
         var function = ffi.TryGetFunction("function_internal_3");
         function.Should().BeNull();
     }
 
-    private void Function4DoesNotExist(CTestFfiTargetPlatform ffi)
+    private void Function4DoesNotExist(CTestFfiCrossPlatform ffi)
     {
         var function = ffi.TryGetFunction("function_internal_4");
         function.Should().BeNull();
     }
 
-    private void Function5DoesExist(CTestFfiTargetPlatform ffi)
+    private void Function5DoesExist(CTestFfiCrossPlatform ffi)
     {
         var function = ffi.TryGetFunction("function_internal_5");
         function.Should().NotBeNull();
